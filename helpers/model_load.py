bbf808626b71bc4914556cf42e0ea8451fd21dce
unfoldingpast <unfoldingpast@gmail.com>
2025-04-22 10:59:07 +0000

    removed model authentication

--- helpers/model_load.py
+++ helpers/model_load.py
@@ -126,42 +126,42 @@ def load_model(root, load_on_run_all=True, check_sha256=True, map_location="cuda
         "sd-v1-4-full-ema.ckpt": {
             'sha256': '14749efc0ae8ef0329391ad4436feb781b402f4fece4883c7ad8d10556d8a36a',
             'url': 'https://huggingface.co/CompVis/stable-diffusion-v-1-2-original/blob/main/sd-v1-4-full-ema.ckpt',
-            'requires_login': True,
+            'requires_login': False,
             },
         "sd-v1-4.ckpt": {
             'sha256': 'fe4efff1e174c627256e44ec2991ba279b3816e364b49f9be2abc0b3ff3f8556',
             'url': 'https://huggingface.co/CompVis/stable-diffusion-v-1-4-original/resolve/main/sd-v1-4.ckpt',
-            'requires_login': True,
+            'requires_login': False,
             },
         "sd-v1-3-full-ema.ckpt": {
             'sha256': '54632c6e8a36eecae65e36cb0595fab314e1a1545a65209f24fde221a8d4b2ca',
             'url': 'https://huggingface.co/CompVis/stable-diffusion-v-1-3-original/blob/main/sd-v1-3-full-ema.ckpt',
-            'requires_login': True,
+            'requires_login': False,
             },
         "sd-v1-3.ckpt": {
             'sha256': '2cff93af4dcc07c3e03110205988ff98481e86539c51a8098d4f2236e41f7f2f',
             'url': 'https://huggingface.co/CompVis/stable-diffusion-v-1-3-original/resolve/main/sd-v1-3.ckpt',
-            'requires_login': True,
+            'requires_login': False,
             },
         "sd-v1-2-full-ema.ckpt": {
             'sha256': 'bc5086a904d7b9d13d2a7bccf38f089824755be7261c7399d92e555e1e9ac69a',
             'url': 'https://huggingface.co/CompVis/stable-diffusion-v-1-2-original/blob/main/sd-v1-2-full-ema.ckpt',
-            'requires_login': True,
+            'requires_login': False,
             },
         "sd-v1-2.ckpt": {
             'sha256': '3b87d30facd5bafca1cbed71cfb86648aad75d1c264663c0cc78c7aea8daec0d',
             'url': 'https://huggingface.co/CompVis/stable-diffusion-v-1-2-original/resolve/main/sd-v1-2.ckpt',
-            'requires_login': True,
+            'requires_login': False,
             },
         "sd-v1-1-full-ema.ckpt": {
             'sha256': 'efdeb5dc418a025d9a8cc0a8617e106c69044bc2925abecc8a254b2910d69829',
             'url':'https://huggingface.co/CompVis/stable-diffusion-v-1-1-original/resolve/main/sd-v1-1-full-ema.ckpt',
-            'requires_login': True,
+            'requires_login': False,
             },
         "sd-v1-1.ckpt": {
             'sha256': '86cd1d3ccb044d7ba8db743d717c9bac603c4043508ad2571383f954390f3cea',
             'url': 'https://huggingface.co/CompVis/stable-diffusion-v-1-1-original/resolve/main/sd-v1-1.ckpt',
-            'requires_login': True,
+            'requires_login': False,
             },
         "robo-diffusion-v1.ckpt": {
             'sha256': '244dbe0dcb55c761bde9c2ac0e9b46cc9705ebfe5f1f3a7cc46251573ea14e16',
